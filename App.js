197c01d2fe97c2db59af91e286fa9439bc4f1c15
AndreasHaavik <72740378+AndreasHaavik@users.noreply.github.com>
2022-12-08 11:36:27 +0100

    import af database
    import af database

--- App.js
+++ App.js
@@ -1,14 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
+import React, {useEffect} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+//import firebase from "firebase";
+import firebase from "firebase/compat";
+import {NavigationContainer} from "@react-navigation/native";
+import {createStackNavigator} from "@react-navigation/stack";
+import {createBottomTabNavigator} from "@react-navigation/bottom-tabs";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCjjBmD6q-n9nVL1D_bC5W9vnDkMZXFF4A",
+  authDomain: "innt-eks.firebaseapp.com",
+  databaseURL: "https://innt-eks-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "innt-eks",
+  storageBucket: "innt-eks.appspot.com",
+  messagingSenderId: "949004649498",
+  appId: "1:949004649498:web:a2dc26943ce9816733fda3"
+};
+
 
 const styles = StyleSheet.create({
   container: {
