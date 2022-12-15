12d113f40464fd379669a1d78135732686b7230c
AndreasHaavik <72740378+AndreasHaavik@users.noreply.github.com>
2022-12-15 12:01:07 +0100

    kommentarer

--- App.js
+++ App.js
@@ -6,10 +6,10 @@ import firebase from "firebase/compat";
 import {NavigationContainer} from "@react-navigation/native";
 import MyTabs from "./Components/TabNavigator";
 import BottomStack from "./Components/StackNavigator";
+//Importere MyTabs og ButtomStack som indeholder applikationen funktionalitet.
 
 
-
-// Your web app's Firebase configuration
+// Configurationen til firebase databasen & Authentication
 const firebaseConfig_database = {
     apiKey: "AIzaSyCjjBmD6q-n9nVL1D_bC5W9vnDkMZXFF4A",
     authDomain: "innt-eks.firebaseapp.com",
@@ -24,16 +24,17 @@ const firebaseConfig_database = {
 
 export default function App() {
 
-    //Oprettelse af statevariablen til brugere
+//Oprettelse af tilstandvariablerne til brugere
       const [user, setUser] = useState({ loggedIn: false });
 
-      //Her anvendes initaliseApp for at sikre at kun en initieres når man bruger firebase
+
+// InitializeApp anvendes for at sikre at man kun initiere en bruger når man anvender firebase
       if (!firebase.apps.length) {
         firebase.initializeApp(firebaseConfig_database);
       }
 
     //onAuthstatechanged er metode af firebase, som har til formål at overvåge brugerens tilstand (logget ind vs logget ud)
-    //Pba. brugerens status foretages et callback i form af setUSer metoden, som håndterer user-state variablens status.
+    //Pba. brugerens status foretages et callback i if/else statement. Enten er brugeren logget ind = rigtigt, ellers (else) logget ind = falsk.
       function onAuthStateChange(callback) {
         return firebase.auth().onAuthStateChanged(user => {
           if (user) {
@@ -44,7 +45,8 @@ export default function App() {
         });
       }
 
-      //onAuthStateChanged aktiveres, så vi nu kan dynamisk observerer om brugeren er aktiv eller ej.
+//onAuthStateChanged aktiveres som kommer fra ovenstående kode.
+// den undersøger derfor om brugeren er logget ind og aktiv eller ikke aktiv
       useEffect(() => {
         const unsubscribe = onAuthStateChange(setUser);
         return () => {
@@ -53,6 +55,9 @@ export default function App() {
       }, []);
 
 
+//Nedenstående er GuestPage og HomePage.
+//GuestPage vises hvis man ikke er logget ind og viser derfor funktionen "MyStack
+//"MyTabs" er en stack af Login, SignUp og HomeScreen
     const GuestPage = () => {
         return(
             <NavigationContainer>
@@ -61,6 +66,10 @@ export default function App() {
         )
       }
 
+//Når man logger ind vises HomePage, som viser BottomStack
+//BottomStack er en Stack af vores funktionalitet (CRUD-endpoints) samt en tab som gør det muligt at navigere mellem liste af lejligheder
+// tilføje en lejlighed samt chatte.
+
     const HomePage = () => {
     return(
        <NavigationContainer>
@@ -68,7 +77,7 @@ export default function App() {
        </NavigationContainer>
     )
   }
-
+//Her returnere vi enten HomePage (når man er logget ind) Eller GuestPage (hvor man enten logger ind eller opretter en bruger)
         return user.loggedIn ? <HomePage /> : <GuestPage/> ;
 }
 
--- Components/TabNavigator.js
+++ Components/TabNavigator.js
@@ -2,13 +2,22 @@ import * as React from "react";
 import {createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { createStackNavigator} from "@react-navigation/stack";
 import {StyleSheet, Text, View} from "react-native";
+//Ovenstående nødvendigt for at kunne lave funktionaliteten
 
 import SignUpForm from "./SignUpForm";
 import LoginForm from "./LoginForm";
 import HomeScreen from "./HomeScreen";
+//Importere 3 funktioner for at kunne lave en hjemmeskærm for derefter at vælge mellem at logge ind eller registrere en bruger
+
+
 
 const Tab = createBottomTabNavigator()
+//Stack consten bruges for at kunne lave en stack mellem HomeScreen, LogIn og SignUp
 const Stack = createStackNavigator()
+
+//"MyTabs" er en funktion som laver en stack mellem HomeScreen, LogIn og SignUp.
+//I HomeScreen er der lokal styling
+//Kort fortalt gør funktionen det muligt at navigere mellem LogIn og SignUp via HomeScreen
 
 function MyTabs (){
     return(
